056d6426bea2b3e3d3dfd4992e9a22af0442e537
Dadi Karthik <105288971+dadikarthik@users.noreply.github.com>
2023-05-08 12:29:33 +0530

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -2,5 +2,5 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast menu')
-streamlit.paragraph('Boiled Eggs')
+streamlit.text('Boiled Eggs')
 streamlit.text('Brockly')
